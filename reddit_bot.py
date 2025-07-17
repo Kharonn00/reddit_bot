ec525e6f583cdf5febe458369bf78f3660a470f2
Kharonn00 <118873120+Kharonn00@users.noreply.github.com>
2025-07-17 18:06:05 -0400

    Update reddit_bot.py

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,30 +1,84 @@
 import praw
 import config
 import time
+import os
+import requests
 
 def bot_login():
-	print "Logging in..."
+    print("Logging in...")
 
-	r = praw.Reddit(username = config.username,
-					password = config.password,
-					client_id = config.client_id,
-					client_secret = config.client_secret,
-					user_agent = "Kharoon elephant comment responder v0.1")
+    reddit = praw.Reddit(
+        username=config.username,
+        password=config.password,
+        client_id=config.client_id,
+        client_secret=config.client_secret,
+        user_agent="Kharoon joke comment responder v0.1"
+    )
 
-	print "Logged in!!"
+    print("Logged in!")
+    return reddit
 
-	return r
 
-def run_bot(r):
-	print "Obtaining 25 comments..."
-	for comment in r.subreddit('test').comments(limit=25):
-		if "elephant" in comment.body:
-			print "String with \"elephant\" found in comment" + comment.id
-			comment.reply("I also love elephants! [Here] (https://en.wikipedia.org/wiki/File:African_Bush_Elephant.jpg) is an image of one!")
-			print "Replied to comment " + comment.id
-			
-	print "Sleeping for 10 secs"
-	time.sleep(10)
+def run_bot(reddit, replied_to):
+    print("Checking latest comments...")
 
-r = bot_login()
-run_bot(r)
+    for comment in reddit.subreddit('test').comments(limit=25):
+
+        if "!joke" in comment.body.lower() and comment.id not in replied_to and comment.author != reddit.user.me():
+            print(f'String with "!joke" found in comment {comment.id}')
+
+            # Try to get a joke from the API
+            try:
+                response = requests.get('http://api.icndb.com/jokes/random', timeout=5)
+                response.raise_for_status()  # raises error for bad responses
+                joke = response.json()['value']['joke']
+            except Exception as e:
+                joke = "Oops! Couldn't fetch a Chuck Norris joke right now. Try again later."
+                print(f"Error fetching joke: {e}")
+
+            comment_reply = f"You requested a Chuck Norris !joke. Here it is:\n\n> {joke}"
+
+            try:
+                comment.reply(comment_reply)
+                print(f"Replied to comment {comment.id}")
+                replied_to.add(comment.id)
+            except Exception as e:
+                print(f"Error replying to comment {comment.id}: {e}")
+
+    # Without API
+    # _______________
+    #
+    # for comment in reddit.subreddit('test').comments(limit=25):
+    #
+    #     if "elephant" in comment.body.lower() and comment.id not in replied_to and comment.author != reddit.user.me():
+    #         print(f"Found 'elephant' in comment {comment.id}")
+    #         comment.reply("I also love elephants! [Here](https://en.wikipedia.org/wiki/File:African_Bush_Elephant.jpg) is an image of one!"
+    #
+    #         print(f"Replied to comment {comment.id}")
+    #         replied_to.add(comment.id)
+
+
+def get_saved_comments():
+    if not os.path.isfile("comment_replied_to.txt"):
+        return set()
+
+    with open("comment_replied_to.txt", "r") as f:
+        ids = f.read().splitlines()
+
+    return set(ids)
+
+
+def save_replied_comments(replied_to):
+    with open("comment_replied_to.txt", "w") as f:
+        for comment_id in replied_to:
+            f.write(comment_id + "\n")
+
+if __name__ == "__main__":
+    reddit = bot_login()
+    replied_to = get_saved_comments()
+
+    while True:
+        run_bot(reddit, replied_to)
+        save_replied_comments(replied_to)
+        print("Sleeping for 10 seconds...\n")
+        time.sleep(10)
